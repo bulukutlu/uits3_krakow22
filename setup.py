7dc0ade1dce29d464066adffe7432fbcd7b56612
Berkin Ulukutlu <berkin.ulukutlu@tum.de>
2023-01-17 10:48:49 +0100

    Filled setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    author_email='berkin.ulukutlu@cern.ch',
+    author='Berkin Ulukutlu',
+    url='https://github.com/bulukutlu/uits3-krakow22',
+    name='uits3-krakow22',
+    version='v0.1', 
+    packages=setuptools.find_packages(),
+    license='MIT License',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=False,
+    package_data={
+    '': ['../*/*/*/*.ts']
+    },
+    install_requires=[
+        'numpy',
+        'pandas',
+        ##---------------------   graphics  dependencies
+        # ----------------------   jupyter notebook dependencies
+        'ipywidgets',
+        'runtime',
+        'requests',
+        "notebook>=6.4.10"
+    ]
+)
