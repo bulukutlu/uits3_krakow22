abfc42b721e1d8a5d8c0098f85d061c64b86c092
Berkin Ulukutlu <berkin.ulukutlu@tum.de>
2023-02-09 15:37:47 +0100

    Fixing event reader

--- uits3_krakow22/src/Utils.py
+++ uits3_krakow22/src/Utils.py
@@ -1,14 +1,17 @@
 import pickle
 
 def readEvents(filename,nEvents=-1):
+    if nEvents == -1 : print("Going to process all events.")
+    else: print("Going to process",nEvents,"events.")
     i = 0
     with open(filename, "rb") as f:
         while True:
             try:
                 events = pickle.load(f)
                 for event in events:
-                    if nEvents == -1 or i < nEvents:
-                        yield event
+                    if nEvents == -1 or i < int(nEvents) :
                         i += 1
-            except EOFError:
-                break
+                        yield event
+                    else:
+                        return
+            except EOFError : break
